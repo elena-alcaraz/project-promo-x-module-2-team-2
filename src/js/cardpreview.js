324b2b9124e24f6bc5c4fecd6d087d05c21d6f23
FatimaDuret <f.duret.g@gmail.com>
2024-04-15 12:57:58 +0200

    inicio objeto

--- src/js/cardpreview.js
+++ src/js/cardpreview.js
@@ -1,11 +1,24 @@
+
 const btnReset = document.querySelector(".js_reset");
 
-btnReset.addEventListener("click", (event) => {
+
+function resetForm(event)  {
+    event.preventDefault();
+    form.reset();
+    namePreview.innerHTML = "Nombre Apellido";
+    jobPreview.innerHTML = "Front-end developer";
+    cardColor.classList.remove('palette2', 'palette3');
+    cardColor.classList.add('palette1');
+};
+
+btnReset.addEventListener("click", resetForm);
+
+/*btnReset.addEventListener("click", (event) => {
     event.preventDefault();
     console.log("estoy cancelando");
     form.reset();
-})
+}) */
 
 
-
+// btnReset.addEventListener("click", nombre de la funcion)
 
